4108d589b75a25d96afbc79810dcb90cbbd210bf
Travis Pinson <travispinson@gmail.com>
2020-11-18 11:13:32 -0600

    Completed fizzbuzz JS to C#

---
+++ fizzbuzz/csharp/fizzbuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
